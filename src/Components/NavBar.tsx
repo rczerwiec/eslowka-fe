9d31c642a561e6c0c9b96b8be6dc87a58a7fe6f8
rczerwiec <czerwiecr1999@gmail.com>
2024-07-07 16:54:30 +0200

    NavBar refactor

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -5,35 +5,49 @@ import { FaChevronDown } from "react-icons/fa";
 const NavBar = () => {
     return(
         <div className="flex absolut top-0 right-0 bg-main w-full h-14 justify-between items-center">
-            <div className="text-white font-bold font-inter text-3xl
-                              pl-4">ESłówka.pl</div>
+
+            <Logo/>
             <div className="flex items-center justify-between
                           bg-secondary 
                             h-full w-56 rounded-tl-xl rounded-bl-xl">
-
-            <div className="flex items-center ml-6">
-                <div className="font-bold font-inter text-2xl">
-                    3
-                </div>
-                <div>
-                    <FaFire className="text-xl"/>
-                </div>
-            </div>
-
-            <div className="flex items-center mr-4">
-                <div className="font-bold font-inter text-xl text-white mr-2">
-                    Radek
-                </div>
-                <div>
-                    <FaUser className="text-2xl"/>
-                </div>       
-                <div>
-                    <FaChevronDown className="text-lg mr-2" />
-                </div>
-            </div>
-
+            <Streak/>
+            <UserMenu />
             </div>
         </div>
+    )
+}
+
+const Streak = () => {
+    return (
+        <div className="flex items-center ml-6">
+        <div className="font-bold font-inter text-2xl">
+            3
+        </div>
+        <div>
+            <FaFire className="text-xl"/>
+        </div>
+    </div>
+    )
+}
+const UserMenu = () => {
+    return (
+        <div className="flex items-center mr-4">
+        <div className="font-bold font-inter text-xl text-white mr-2">
+            Radek
+        </div>
+        <div>
+            <FaUser className="text-2xl"/>
+        </div>       
+        <div>
+            <FaChevronDown className="text-lg mr-2" />
+        </div>
+    </div>
+    )
+}
+const Logo = () => {
+    return(
+        <div className="text-white font-bold font-inter text-3xl
+        pl-4">ESłówka.pl</div>
     )
 }
 
