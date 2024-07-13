78cf1bc0d5e6c9a439c8d03623550fa2c0abe6cd
cyani99 <czerwiecr1999@icloud.com>
2024-07-13 10:40:56 +0200

    added react router

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 
 import {Provider} from "react-redux"
 import { store } from './shared/store';
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import NavBar from './shared/components/UI/NavBar';
+import SideBar from './shared/components/UI/SideBar';
+import FoldersPage from './views/Folders/FoldersPage';
+import WordsInFolderPage from './views/Folders/WordsInFolderPage';
+
+const router = createBrowserRouter([
+  {
+    path: "/folders",
+    element: <FoldersPage/>,
+  },
+  {
+    path: "/folders/words",
+    element: <WordsInFolderPage/>,
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +31,13 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-       <App/>
+    <div className="flex flex-col h-screen">
+      <NavBar></NavBar>
+      <div className="flex h-full">
+          <SideBar></SideBar>
+          <RouterProvider router={router} />
+      </div>
+    </div>
     </Provider>
   </React.StrictMode>
 );
